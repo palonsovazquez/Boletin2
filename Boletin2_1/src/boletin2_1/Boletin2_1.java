13d2d69555de4f3f25dd5a674d56fc106acc7a4b
palonsovazquez <>
2018-10-09 12:03:58 +0200

    terminado Boletin2_1

--- Boletin2_1/src/boletin2_1/Boletin2_1.java
+++ Boletin2_1/src/boletin2_1/Boletin2_1.java
@@ -5,17 +5,60 @@
  */
 package boletin2_1;
 
+
+import java.util.Scanner;
+
 /**
  *
  * @author Pablo Alonso Vazquez <pav.vigo@gmail.com>
  */
 public class Boletin2_1 {
-
+public static Scanner sca = new Scanner(System.in);
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Double precioOrg, precioFin, porcentaje ;
+        
+        do{
+        precioOrg = pedirNumero("el precio original");
+        precioFin = pedirNumero("el precio Final");
+        }while(precioOrg < precioFin ||0== precioOrg );
+        System.out.println("Precio Original: "+ precioOrg);
+        System.out.println("Precio Final   : "+ precioFin);
+        porcentaje = 100 -100*precioFin/precioOrg;
+        System.out.println("Descuento      : "+porcentaje + " %");
+        
+        
     }
+    public static Double pedirNumero(String nombreDato){
+    Double res;
+        do {
+    System.out.print("Introduzca "+ nombreDato+": ");
+    res = comprobarNum( sca.nextLine());
+    System.out.println();
+    }while(res == null);  // pedira el dato hasta que e introduzca un numero valido.
+    return res;
     
+    
+    }
+     public static  Double comprobarNum(String numero){
+         Double x ;
+        try{   // lo parseo y si no es un numero valido asigna a x el valor de nulo.
+           x =  Double.parseDouble(numero); 
+        }catch(Exception ex){
+            x=null;
+            
+     }
+      
+            
+            
+            return x;
+      
+        
+        
+
+
+
+}    
 }
